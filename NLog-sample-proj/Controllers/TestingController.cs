f0e2b59d1e02cd3c1eb152d395372c99369f8c44
Abiraj UCA <abiraj@unicornconnectedapps.com>
2025-03-15 23:44:14 +0530

    Add project files.

---
+++ NLog-sample-proj/Controllers/TestingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace NLog_sample_proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+
+        [HttpGet("debug")]
+        public IActionResult LogDebug()
+        {
+            _logger.Debug("This is a DEBUG log.");
+            return Ok("DEBUG log recorded.");
+        }
+
+        [HttpGet("info")]
+        public IActionResult LogInfo()
+        {
+            _logger.Info("This is an INFO log.");
+            return Ok("INFO log recorded.");
+        }
+
+        [HttpGet("warn")]
+        public IActionResult LogWarning()
+        {
+            _logger.Warn("This is a WARNING log.");
+            return Ok("WARNING log recorded.");
+        }
+
+        [HttpGet("error")]
+        public IActionResult LogError()
+        {
+            try
+            {
+                throw new Exception("Sample exception for logging.");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "An ERROR occurred.");
+            }
+            return BadRequest("ERROR log recorded.");
+        }
+    }
+}
---
+++ NLog-sample-proj/Program.cs
@@ -0,0 +1,42 @@
+
+using NLog.Web;
+
+namespace NLog_sample_proj
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Use NLog as the logging provider
+            builder.Logging.ClearProviders();
+            builder.Host.UseNLog();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
